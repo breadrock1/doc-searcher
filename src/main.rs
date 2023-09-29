9d0abd728aff1c4f67f3b16b1c839ba6f0c91c86
Bread White <breadrock1@gmail.com>
2023-09-29 14:57:08 +0300

    chore(main): add building elastic client method

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,20 @@ mod endpoints;
 mod errors;
 mod wrappers;
 
-use actix_web::{web, App, HttpServer, Scope};
 use crate::context::SearchContext;
 use crate::endpoints::elastic::{create_index, find_index};
 
+use actix_web::{web, App, HttpServer, Scope};
+use elasticsearch::{Elasticsearch, SearchParts};
+use elasticsearch::auth::Credentials;
+use elasticsearch::cert::CertificateValidation;
+use elasticsearch::http::transport::{SingleNodeConnectionPool, Transport, TransportBuilder};
+use elasticsearch::http::Url;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let elastic_addr = "https://localhost:9200";
-    let cxt = SearchContext::_new(elastic_addr).unwrap();
+    let elastic = build_elastic().unwrap();
+    let cxt = SearchContext::_new(elastic).unwrap();
     HttpServer::new(move || {
         let cxt = cxt.clone();
         App::new()
@@ -21,6 +26,20 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 45678))?
     .run()
     .await
+}
+
+fn build_elastic() -> Option<Elasticsearch> {
+    let es_url = Url::parse("https://localhost:9200").unwrap();
+    let conn_pool = SingleNodeConnectionPool::new(es_url);
+    let creds = Credentials::Basic("elastic".into(), "MomQSpuUJk0ANHBTjSKM".into());
+    let validation = CertificateValidation::None;
+    let transport = TransportBuilder::new(conn_pool)
+        .auth(creds)
+        .cert_validation(validation)
+        .build()
+        .unwrap();
+
+    Some(Elasticsearch::new(transport))
 }
 
 fn build_service() -> Scope {
