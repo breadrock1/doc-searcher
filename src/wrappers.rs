d18def6fd80381bf62c88992c2af51160898abb2
Bread White <breadrock1@gmail.com>
2023-10-06 01:39:55 +0300

    feat(searcher)!: add searcher wrappers

--- src/wrappers.rs
+++ src/wrappers.rs
@@ -1,5 +1,129 @@
-use std::os::linux::raw::stat;
-use serde::{Deserialize, Serialize};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize, Serializer};
+use serde_json::Value;
+
+#[derive(Serialize)]
+#[serde(rename = "document_size")]
+pub struct DocumentSizeQuery {
+    gte: i64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    lte: Option<i64>,
+}
+
+impl DocumentSizeQuery {
+    pub fn new(gte: i64, lte: i64) -> Self {
+        let lte_value = match lte > 0 {
+            true => Some(lte),
+            false => None,
+        };
+
+        DocumentSizeQuery {
+            gte,
+            lte: lte_value,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct QueryString {
+    query: String,
+}
+
+impl QueryString {
+    pub fn new(value: String) -> Self {
+        QueryString { query: value }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MultiMatchQuery {
+    multi_match: QueryString,
+}
+
+impl MultiMatchQuery {
+    pub fn new(value: QueryString) -> Self {
+        MultiMatchQuery { multi_match: value }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MustQuery {
+    must: Value,
+}
+
+impl MustQuery {
+    pub fn new(value: Value) -> Self {
+        MustQuery { must: value }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct BoolQuery {
+    #[serde(alias = "bool")]
+    bool: MustQuery,
+}
+
+impl BoolQuery {
+    pub fn new(value: MustQuery) -> Self {
+        BoolQuery { bool: value }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RangeQuery {
+    range: Value,
+}
+
+impl RangeQuery {
+    pub fn new(value: Value) -> Self {
+        RangeQuery { range: value }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SearchQuery {
+    bool: MustQuery,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    filter: Option<BoolQuery>,
+}
+
+impl SearchQuery {
+    pub fn new1(match_query: MustQuery) -> Self {
+        SearchQuery {
+            bool: match_query,
+            filter: None,
+        }
+    }
+
+    pub fn new2(match_query: MustQuery, filter_query: BoolQuery) -> Self {
+        SearchQuery {
+            bool: match_query,
+            filter: Some(filter_query),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MainQueryNode {
+    query: SearchQuery,
+}
+
+impl MainQueryNode {
+    pub fn new(value: SearchQuery) -> Self {
+        MainQueryNode { query: value }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SearchParameters {
+    pub query: String,
+    pub document_size_to: i64,
+    pub document_size_from: i64,
+    pub created_date_to: i64,
+    pub created_date_from: i64,
+    pub result_size: i64,
+    pub result_offset: i64,
+}
 
 #[derive(Deserialize, Serialize)]
 pub struct Document {
