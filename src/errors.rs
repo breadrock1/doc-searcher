1334e9cd5d682e9e79e652f607ca6d544013f9ae
Bread White <breadrock1@gmail.com>
2023-10-07 20:41:54 +0300

    chore(errors): update WebError enums

--- src/errors.rs
+++ src/errors.rs
@@ -6,21 +6,22 @@ use thiserror::Error;
 
 pub type WebResponse<T> = Result<T, WebError>;
 
-#[derive(Serialize)]
-struct ErrorResponse {
-    code: u16,
-    error: String,
-    message: String,
-}
-
 #[derive(Error, Debug)]
 pub enum WebError {
     #[error("Unknown error: {0}")]
     RuntimeError(String),
     #[error("Error while getting cluster: {0}")]
     GetClusterError(String),
+    #[error("Error while creating cluster: {0}")]
+    CreateClusterError(String),
+    #[error("Error while deleting cluster: {0}")]
+    DeletingClusterError(String),
     #[error("Error while getting bucket: {0}")]
     GetBucketError(String),
+    #[error("Error while creating bucket: {0}")]
+    CreateBucketError(String),
+    #[error("Error while deleting bucket: {0}")]
+    DeleteBucketError(String),
     #[error("Error elastic response: {0}")]
     GetDocumentError(String),
     #[error("Failed while creating document: {0}")]
@@ -33,23 +34,25 @@ pub enum WebError {
     DocumentSerializingError(String),
     #[error("Failed while searching: {0}")]
     SearchError(String),
+    #[error("Response error: {0}")]
+    ResponseError(String),
+    #[error("Failed while parsing bucket: {0}")]
+    BucketParsingError(String),
 }
 
 impl WebError {
-    pub fn new(msg: String) -> Self {
-        WebError::RuntimeError(msg)
-    }
-
     pub fn name(&self) -> String {
         match self {
             WebError::SearchError(_) => "SearchError",
-            WebError::GetClusterError(_) => "GetClusterError",
+            WebError::ResponseError(_) => "ResponseError",
             WebError::GetBucketError(_) => "GetBucketError",
+            WebError::GetClusterError(_) => "GetClusterError",
             WebError::GetDocumentError(_) => "GetDocumentError",
             WebError::CreateDocumentError(_) => "CreateDocumentError",
             WebError::UpdateDocumentError(_) => "UpdateDocumentError",
             WebError::DeleteDocumentError(_) => "DeleteDocumentError",
             WebError::DocumentSerializingError(_) => "DocumentSerializingError",
+            WebError::BucketParsingError(_) => "BucketParsingError",
             _ => "RuntimeError",
         }
         .to_string()
@@ -58,16 +61,24 @@ impl WebError {
 
 impl From<elasticsearch::Error> for WebError {
     fn from(value: Error) -> Self {
-        WebError::new(value.to_string())
+        WebError::ResponseError(value.to_string())
     }
+}
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    code: u16,
+    error: String,
+    message: String,
 }
 
 impl ResponseError for WebError {
     fn status_code(&self) -> StatusCode {
         match self {
             WebError::SearchError(_) => StatusCode::BAD_REQUEST,
-            WebError::GetClusterError(_) => StatusCode::BAD_REQUEST,
+            WebError::ResponseError(_) => StatusCode::BAD_REQUEST,
             WebError::GetBucketError(_) => StatusCode::BAD_REQUEST,
+            WebError::GetClusterError(_) => StatusCode::BAD_REQUEST,
             WebError::GetDocumentError(_) => StatusCode::BAD_REQUEST,
             WebError::CreateDocumentError(_) => StatusCode::BAD_REQUEST,
             WebError::UpdateDocumentError(_) => StatusCode::BAD_REQUEST,
