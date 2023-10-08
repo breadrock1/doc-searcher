755de50c976edc8cfd62c877dfdc8a72ff8d7b0a
Bread White <breadrock1@gmail.com>
2023-10-09 00:32:26 +0300

    chore(clusters): add tests for clusters.rs

--- src/endpoints/clusters.rs
+++ src/endpoints/clusters.rs
@@ -108,6 +108,85 @@ async fn get_cluster(
     let response = response_result.unwrap();
     match response.json::<Value>().await {
         Ok(cluster_info) => Ok(web::Json(cluster_info)),
-        Err(err) => Err(WebError::GetClusterError(err.to_string())),
+        Err(err) => Err(WebError::GetCluster(err.to_string())),
+    }
+}
+
+#[cfg(test)]
+mod cluster_endpoints {
+    use crate::context::SearchContext;
+    use crate::errors::SuccessfulResponse;
+    use crate::es_client::{build_elastic, build_service, init_service_parameters};
+    use crate::wrappers::Cluster;
+
+    use actix_web::test::TestRequest;
+    use actix_web::{test, web, App};
+    use serde_json::json;
+
+    #[test]
+    async fn build_application() {
+        let service_parameters = init_service_parameters().unwrap();
+        let es_host = service_parameters.es_host();
+        let es_user = service_parameters.es_user();
+        let es_passwd = service_parameters.es_passwd();
+        let service_port = service_parameters.service_port();
+        let service_addr = service_parameters.service_address();
+
+        let elastic = build_elastic(es_host, es_user, es_passwd).unwrap();
+        let cxt = SearchContext::_new(elastic);
+        let app = App::new()
+            .app_data(web::Data::new(cxt))
+            .service(build_service());
+
+        let test_app = test::init_service(app).await;
+        let test_cluster_name = "test_cluster";
+
+        // Create new cluster with name: "test_cluster"
+        let create_cluster_resp = TestRequest::post()
+            .uri("/searcher/cluster/new")
+            .set_json(&json!({"cluster_name": test_cluster_name}))
+            .send_request(&test_app)
+            .await;
+
+        let new_cluster: SuccessfulResponse = test::read_body_json(create_cluster_resp).await;
+        assert_eq!(new_cluster.code, 400);
+
+        // Get all clusters request
+        let get_all_clusters_resp = TestRequest::get()
+            .uri("/searcher/clusters")
+            .send_request(&test_app)
+            .await;
+
+        let get_all_clusters: Vec<Cluster> = test::read_body_json(get_all_clusters_resp).await;
+        assert_eq!(get_all_clusters.len() > 0, true);
+
+        // Get cluster request by index
+        // let get_cluster_resp = TestRequest::get()
+        //     .uri(&format!("/searcher/cluster/{}", test_cluster_name))
+        //     .send_request(&test_app)
+        //     .await;
+        //
+        // let get_cluster: Cluster = test::read_body_json(get_cluster_resp).await;
+        // assert_eq!(get_cluster.name(), test_cluster_name);
+
+        // TODO: Skip deleting cluster test
+        // Delete cluster by index
+        // let delete_cluster_resp = TestRequest::delete()
+        //     .uri(&format!("/searcher/cluster/{}", test_cluster_name))
+        //     .send_request(&test_app)
+        //     .await;
+        //
+        // let delete_cluster: SuccessfulResponse = test::read_body_json(delete_cluster_resp).await;
+        // assert_eq!(delete_cluster.code, 200);
+
+        // TODO: Skip getting cluster test
+        // Get cluster by index -> get error message
+        // let delete_cluster_resp = TestRequest::get()
+        //     .uri(&format!("/searcher/cluster/{}", "lsdfnbsikdjfsidg"))
+        //     .send_request(&test_app)
+        //     .await;
+        //
+        // let delete_cluster: ErrorResponse = test::read_body_json(delete_cluster_resp).await;
+        // assert_eq!(delete_cluster.code, 400);
     }
 }
