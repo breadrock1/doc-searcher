3dc7720e2533ac81cd2077be1a3daf54e69431b7
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-11-10 16:28:34 +0300

    chore(wrappers): create cluster.rs file with wrappers for cluster endpoint

---
+++ src/wrappers/cluster.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct Cluster {
+    ip: String,
+    #[serde(alias = "heap.percent")]
+    heap_percent: String,
+    #[serde(alias = "ram.percent")]
+    ram_percent: String,
+    cpu: String,
+    load_1m: String,
+    load_5m: String,
+    load_15m: String,
+    #[serde(alias = "node.role")]
+    node_role: String,
+    master: String,
+    name: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct ClusterForm {
+    cluster_name: String,
+}
+
+impl ToString for ClusterForm {
+    fn to_string(&self) -> String {
+        let self_data = &self.cluster_name;
+        self_data.clone()
+    }
+}
