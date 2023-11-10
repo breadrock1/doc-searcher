b15f6cfe5a25d9085ac99878ac9a8c99d75e1635
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-11-10 16:27:00 +0300

    chore(wrappers): move buckets wrappers to own bucket.rs crate

---
+++ src/wrappers/bucket.rs
@@ -0,0 +1,62 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct Bucket {
+    pub health: String,
+    pub status: String,
+    pub index: String,
+    pub uuid: String,
+    #[serde(alias = "docs.count")]
+    pub docs_count: String,
+    #[serde(alias = "docs.deleted")]
+    pub docs_deleted: String,
+    #[serde(alias = "store.size")]
+    pub store_size: String,
+    #[serde(alias = "pri.store.size")]
+    pub pri_store_size: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub pri: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub rep: Option<String>,
+}
+
+#[allow(clippy::too_many_arguments)]
+impl Bucket {
+    pub fn new(
+        health: String,
+        status: String,
+        index: String,
+        uuid: String,
+        docs_count: String,
+        docs_deleted: String,
+        store_size: String,
+        pri_store_size: String,
+        pri: Option<String>,
+        rep: Option<String>,
+    ) -> Self {
+        Bucket {
+            health,
+            status,
+            index,
+            uuid,
+            docs_count,
+            docs_deleted,
+            store_size,
+            pri_store_size,
+            pri,
+            rep,
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct BucketForm {
+    bucket_name: String,
+}
+
+impl ToString for BucketForm {
+    fn to_string(&self) -> String {
+        let self_data = &self.bucket_name;
+        self_data.clone()
+    }
+}
