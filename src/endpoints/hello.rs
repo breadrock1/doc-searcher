f2b5cf3435e627f4150ad51a761b1e72902684eb
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-11-22 11:32:12 +0300

    chore(endpoints): update hello endpoint

--- src/endpoints/hello.rs
+++ src/endpoints/hello.rs
@@ -1,10 +1,10 @@
-use crate::context::SearchContext;
 use crate::errors::SuccessfulResponse;
+use crate::searcher::service_client::ServiceClient;
 
 use actix_web::{get, web, HttpResponse};
 
 #[get("/hello")]
-async fn hello(cxt: web::Data<SearchContext>) -> HttpResponse {
-    let _elastic = cxt.get_cxt().read().await;
+async fn hello(cxt: web::Data<&dyn ServiceClient>) -> HttpResponse {
+    let _client = cxt.get_ref();
     SuccessfulResponse::ok_response("Ok")
 }
