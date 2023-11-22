c6bd2c1eba4b4f0d6a8c16e5325a329a9da85f53
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-11-22 11:24:11 +0300

    chore(endpoints): move functionality fo similarities to own endpoint

---
+++ src/endpoints/similarities.rs
@@ -0,0 +1,134 @@
+use crate::errors::WebResponse;
+use crate::searcher::service_client::ServiceClient;
+use crate::wrappers::document::Document;
+use crate::wrappers::search_params::*;
+
+use actix_web::{post, web};
+
+#[post("/search-similar")]
+async fn search_similar_docs(
+    cxt: web::Data<dyn ServiceClient>,
+    form: web::Json<SearchParameters>,
+) -> WebResponse<web::Json<Vec<Document>>> {
+    let client = cxt.get_ref();
+    let search_form = form.0;
+    client.similar_from_all(&search_form).await
+}
+
+#[post("/search-similar/{bucket_names}")]
+async fn search_similar_docs_target(
+    cxt: web::Data<dyn ServiceClient>,
+    path: web::Path<String>,
+    form: web::Json<SearchParameters>,
+) -> WebResponse<web::Json<Vec<Document>>> {
+    let client = cxt.get_ref();
+    let search_form = form.0;
+    let buckets = path.as_ref();
+    client
+        .similar_from_target(buckets.as_str(), &search_form)
+        .await
+}
+
+#[cfg(test)]
+mod similarities_endpoints {
+    use crate::es_client::{build_elastic, build_service, init_service_parameters};
+    use crate::searcher::elastic::context::ElasticContext;
+    use crate::wrappers::document::Document;
+    use crate::wrappers::search_params::*;
+
+    use actix_web::test::TestRequest;
+    use actix_web::{test, web, App};
+    use serde_json::json;
+
+    #[test]
+    async fn build_application() {
+        let service_parameters = init_service_parameters().unwrap();
+        let es_host = service_parameters.es_host();
+        let es_user = service_parameters.es_user();
+        let es_passwd = service_parameters.es_passwd();
+        let service_port = service_parameters.service_port();
+        let service_addr = service_parameters.service_address();
+
+        let elastic = build_elastic(es_host, es_user, es_passwd).unwrap();
+        let cxt = ElasticContext::_new(elastic);
+        let app = App::new()
+            .app_data(web::Data::new(cxt))
+            .service(build_service());
+
+        let test_app = test::init_service(app).await;
+        let test_bucket_name = "test_bucket";
+        for document_index in 1..5 {
+            let document_size = 1024 + document_index;
+            let test_document_name = &format!("test_document_{}", document_index);
+            let create_document_resp = TestRequest::post()
+                .uri("/searcher/document/new")
+                .set_json(&json!({
+                    "bucket_uuid": test_bucket_name,
+                    "bucket_path": "/tmp/test_document",
+                    "document_name": test_document_name,
+                    "document_path": "/tmp/dir/",
+                    "document_size": document_size,
+                    "document_type": "document",
+                    "document_extension": ".docx",
+                    "document_permissions": 777,
+                    "document_created": "2023-09-15T00:00:00Z",
+                    "document_modified": "2023-09-15T00:00:00Z",
+                    "document_md5_hash": test_document_name,
+                    "document_ssdeep_hash": "3a:34gh5",
+                    "entity_data": "Using skip_serializing does not skip deserializing the field.",
+                    "entity_keywords": ["document", "report"]
+                }))
+                .send_request(&test_app)
+                .await;
+        }
+
+        // Found documents request by document name
+        let mut search_params = SearchParameters::default();
+        search_params.query = "document".to_string();
+        let search_resp = TestRequest::post()
+            .uri("/searcher/search")
+            .set_json(&search_params)
+            .send_request(&test_app)
+            .await;
+
+        let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
+        assert_eq!(founded_documents.len() > 0, true);
+
+        // Found documents request by document name with filter
+        let mut search_params = SearchParameters::default();
+        search_params.query = "document".to_string();
+        search_params.document_size_from = 1026;
+        let search_resp = TestRequest::post()
+            .uri("/searcher/search")
+            .set_json(&search_params)
+            .send_request(&test_app)
+            .await;
+
+        let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
+        assert_eq!(founded_documents.len() >= 1, true);
+
+        // Found documents request by document name and bucket name
+        let mut search_params = SearchParameters::default();
+        search_params.query = "document".to_string();
+        let search_resp = TestRequest::post()
+            .uri(&format!("/searcher/search/{}", test_bucket_name))
+            .set_json(&search_params)
+            .send_request(&test_app)
+            .await;
+
+        let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
+        assert_eq!(founded_documents.len() >= 4, true);
+
+        // Found documents request by document name and bucket name
+        let mut search_params = SearchParameters::default();
+        search_params.query = "does not skip".to_string();
+        let search_resp = TestRequest::post()
+            .uri("/searcher/search")
+            .set_json(&search_params)
+            .send_request(&test_app)
+            .await;
+
+        let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
+        assert_eq!(founded_documents.len() >= 4, true);
+    }
+}
