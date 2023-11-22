359f2f12015b53b571bb0355196eb436cbfa5709
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-11-22 12:21:06 +0300

    fix(endpoints): fix imports after all changes for similarities.rs

--- src/endpoints/similarities.rs
+++ src/endpoints/similarities.rs
@@ -31,7 +31,8 @@ async fn search_similar_docs_target(
 
 #[cfg(test)]
 mod similarities_endpoints {
-    use crate::es_client::{build_elastic, build_service, init_service_parameters};
+    use crate::service::{build_service, init_service_parameters};
+    use crate::searcher::elastic::build_elastic_client;
     use crate::searcher::elastic::context::ElasticContext;
     use crate::wrappers::document::Document;
     use crate::wrappers::search_params::*;
@@ -49,7 +50,7 @@ mod similarities_endpoints {
         let service_port = service_parameters.service_port();
         let service_addr = service_parameters.service_address();
 
-        let elastic = build_elastic(es_host, es_user, es_passwd).unwrap();
+        let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
         let cxt = ElasticContext::_new(elastic);
         let app = App::new()
             .app_data(web::Data::new(cxt))
