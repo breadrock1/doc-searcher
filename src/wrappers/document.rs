54cc845118e27d644853143dfa6c93697a6f4a13
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-11-22 11:30:26 +0300

    chore(wrappers): add Default macro for Document structure

--- src/wrappers/document.rs
+++ src/wrappers/document.rs
@@ -1,7 +1,7 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize, Serializer};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Default)]
 pub struct Document {
     pub bucket_uuid: String,
     pub bucket_path: String,
