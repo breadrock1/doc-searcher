25a139397a4fcf6e0656c5a2916391a37451ce13
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-11-24 12:23:47 +0300

    feat(wrappers): impl file_form.rs file

---
+++ src/wrappers/file_form.rs
@@ -0,0 +1,26 @@
+use actix_multipart::form::tempfile::TempFile;
+use actix_multipart::form::MultipartForm;
+use actix_web::FromRequest;
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(MultipartForm)]
+pub struct UploadFileForm {
+    file: TempFile,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct LoadFileForm {
+    file_path: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    bucket_name: Option<String>,
+}
+
+impl LoadFileForm {
+    pub fn get_path(&self) -> &str {
+        self.file_path.as_str()
+    }
+
+    pub fn get_bucket(&self) -> Option<&String> {
+        self.bucket_name.as_ref()
+    }
+}
