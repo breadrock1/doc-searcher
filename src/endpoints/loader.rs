5ae8afaa16ba14c8d9caac0079bb48f0477edd0b
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-12-14 22:17:27 +0300

    chore(endpoints): removed useless load_file_to_all from lodder endpoints

--- src/endpoints/loader.rs
+++ src/endpoints/loader.rs
@@ -11,29 +11,10 @@ async fn upload_file(cxt: ContextData, _form: MultipartForm<UploadFileForm>) -> 
     SuccessfulResponse::ok_response("Ok")
 }
 
-#[post("/loader/{bucket_name}/upload")]
-async fn upload_file_to_bucket(
-    cxt: ContextData,
-    _form: MultipartForm<UploadFileForm>,
-) -> HttpResponse {
-    let _client = cxt.get_ref();
-    SuccessfulResponse::ok_response("Ok")
-}
-
 #[post("/loader/load")]
 async fn load_file(cxt: ContextData, form: web::Json<LoadFileForm>) -> HttpResponse {
     let client = cxt.get_ref();
     let file_path = form.get_path();
-    client.load_file_to_all(file_path).await
-}
-
-#[post("/loader/{bucket_name}/load")]
-async fn load_file_to_bucket(cxt: ContextData, form: web::Json<LoadFileForm>) -> HttpResponse {
-    let client = cxt.get_ref();
-    let file_path = form.get_path();
-    match form.get_bucket() {
-        None => client.load_file_to_all(file_path),
-        Some(bucket) => client.load_file_to_bucket(bucket, file_path),
-    }
-    .await
+    let bucket_name = form.get_bucket();
+    client.load_file_to_bucket(bucket_name, file_path).await
 }
