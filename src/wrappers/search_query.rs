6fa6f91ac9ddaa0eb0e5cf289b8b69cbf8fb9cab
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-12-18 19:50:12 +0300

    feat(wrappers): added additional fields to QueryString structure

--- src/wrappers/search_query.rs
+++ src/wrappers/search_query.rs
@@ -3,12 +3,19 @@ use serde_derive::Serialize;
 #[derive(Serialize)]
 struct QueryString {
     query: String,
+    operator: String,
+    fields: Vec<String>,
 }
 
 impl QueryString {
     pub fn new(value: &str) -> Self {
         QueryString {
             query: value.to_string(),
+            operator: "or".to_string(),
+            fields: vec![
+                "entity_data".to_string(),
+                "document_path".to_string(),
+            ]
         }
     }
 }
