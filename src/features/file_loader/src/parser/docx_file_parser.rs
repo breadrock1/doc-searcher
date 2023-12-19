e58eb02523e6029850e59d4f5344587ae122ff28
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-12-19 23:17:38 +0300

    chore(file_loader): impled docx_file_parser.rs

---
+++ src/features/file_loader/src/parser/docx_file_parser.rs
@@ -0,0 +1,17 @@
+use docx_rust::DocxFile;
+use std::io::Error;
+use std::path::Path;
+
+pub fn parse(file_path: &Path) -> Result<String, Error> {
+    let docx = DocxFile::from_file(file_path).unwrap();
+    let docx = docx.parse().unwrap();
+    let entity_data = docx.document.body.text();
+    Ok(entity_data)
+}
+
+#[test]
+pub fn parse_test() {
+    let file_path = Path::new("/Users/breadrock/Downloads/Job-offer.docx");
+    let data= parse(file_path).unwrap();
+    println!("{}", data);
+}
