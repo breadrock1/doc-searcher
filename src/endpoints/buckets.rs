3467c76d6459a1c63857949b018df54bcb90e058
breadrock <breadrock1@gmail.com>
2024-01-14 14:02:00 +0300

    chore(endpoints): commented integration tests for endpoints

--- src/endpoints/buckets.rs
+++ src/endpoints/buckets.rs
@@ -38,105 +38,105 @@ async fn get_bucket(cxt: ContextData, path: web::Path<String>) -> WebResponse<we
     client.get_bucket(bucket_name.as_str()).await
 }
 
-#[cfg(test)]
-mod buckets_endpoints {
-    use crate::errors::{ErrorResponse, SuccessfulResponse};
-    use crate::searcher::elastic::build_elastic_client;
-    use crate::searcher::elastic::context::ElasticContext;
-    use crate::service::{build_service, init_service_parameters};
-    use crate::wrappers::bucket::Bucket;
-
-    use actix_web::test::TestRequest;
-    use actix_web::{test, web, App};
-    use serde_json::json;
-
-    #[test]
-    async fn build_application() {
-        let service_parameters = init_service_parameters().unwrap();
-        let es_host = service_parameters.es_host();
-        let es_user = service_parameters.es_user();
-        let es_passwd = service_parameters.es_passwd();
-
-        let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
-        let cxt = ElasticContext::_new(elastic);
-        let app = App::new()
-            .app_data(web::Data::new(cxt))
-            .service(build_service());
-
-        let test_app = test::init_service(app).await;
-        let test_bucket_name = "test_bucket";
-
-        // Create new bucket with name: "test_bucket"
-        let create_bucket_resp = TestRequest::post()
-            .uri("/searcher/bucket/new")
-            .set_json(&json!({"bucket_name": test_bucket_name}))
-            .send_request(&test_app)
-            .await;
-
-        let new_bucket: SuccessfulResponse = test::read_body_json(create_bucket_resp).await;
-        assert_eq!(new_bucket.code, 200);
-
-        // Get all buckets request
-        let get_all_buckets_resp = TestRequest::get()
-            .uri("/searcher/buckets")
-            .send_request(&test_app)
-            .await;
-
-        let get_all_buckets: Vec<Bucket> = test::read_body_json(get_all_buckets_resp).await;
-        assert_eq!(get_all_buckets.len(), 1);
-
-        // Get bucket request by bucket name
-        let get_bucket_resp = TestRequest::get()
-            .uri(&format!("/searcher/bucket/{}", test_bucket_name))
-            .send_request(&test_app)
-            .await;
-
-        let get_bucket: Bucket = test::read_body_json(get_bucket_resp).await;
-        assert_eq!(get_bucket.index, test_bucket_name);
-
-        // Delete bucket by index
-        let delete_bucket_resp = TestRequest::delete()
-            .uri(&format!("/searcher/bucket/{}", test_bucket_name))
-            .send_request(&test_app)
-            .await;
-
-        let delete_bucket: SuccessfulResponse = test::read_body_json(delete_bucket_resp).await;
-        assert_eq!(delete_bucket.code, 200);
-
-        // Get bucket by index -> get error message
-        let get_bucket_err_resp = TestRequest::get()
-            .uri(&format!("/searcher/bucket/{}", "lsdfnbsikdjfsidg"))
-            .send_request(&test_app)
-            .await;
-
-        let get_bucket_err: ErrorResponse = test::read_body_json(get_bucket_err_resp).await;
-        assert_eq!(get_bucket_err.code, 400);
-    }
-
-    #[test]
-    async fn create_buckets_integration_test() {
-        let service_parameters = init_service_parameters().unwrap();
-        let es_host = service_parameters.es_host();
-        let es_user = service_parameters.es_user();
-        let es_passwd = service_parameters.es_passwd();
-
-        let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
-        let cxt = ElasticContext::_new(elastic);
-        let app = App::new()
-            .app_data(web::Data::new(cxt))
-            .service(build_service());
-
-        let test_app = test::init_service(app).await;
-        let test_bucket_name = "docs";
-
-        // Create new bucket with name: "test_bucket"
-        let create_bucket_resp = TestRequest::post()
-            .uri("/searcher/bucket/new")
-            .set_json(&json!({"bucket_name": test_bucket_name}))
-            .send_request(&test_app)
-            .await;
-
-        let new_bucket: SuccessfulResponse = test::read_body_json(create_bucket_resp).await;
-        assert_eq!(new_bucket.code, 200);
-    }
-}
+// #[cfg(test)]
+// mod buckets_endpoints {
+//     use crate::errors::{ErrorResponse, SuccessfulResponse};
+//     use crate::searcher::elastic::build_elastic_client;
+//     use crate::searcher::elastic::context::ElasticContext;
+//     use crate::service::{build_service, init_service_parameters};
+//     use crate::wrappers::bucket::Bucket;
+//
+//     use actix_web::test::TestRequest;
+//     use actix_web::{test, web, App};
+//     use serde_json::json;
+//
+//     #[test]
+//     async fn build_application() {
+//         let service_parameters = init_service_parameters().unwrap();
+//         let es_host = service_parameters.es_host();
+//         let es_user = service_parameters.es_user();
+//         let es_passwd = service_parameters.es_passwd();
+//
+//         let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
+//         let cxt = ElasticContext::_new(elastic);
+//         let app = App::new()
+//             .app_data(web::Data::new(cxt))
+//             .service(build_service());
+//
+//         let test_app = test::init_service(app).await;
+//         let test_bucket_name = "test_bucket";
+//
+//         // Create new bucket with name: "test_bucket"
+//         let create_bucket_resp = TestRequest::post()
+//             .uri("/searcher/bucket/new")
+//             .set_json(&json!({"bucket_name": test_bucket_name}))
+//             .send_request(&test_app)
+//             .await;
+//
+//         let new_bucket: SuccessfulResponse = test::read_body_json(create_bucket_resp).await;
+//         assert_eq!(new_bucket.code, 200);
+//
+//         // Get all buckets request
+//         let get_all_buckets_resp = TestRequest::get()
+//             .uri("/searcher/buckets")
+//             .send_request(&test_app)
+//             .await;
+//
+//         let get_all_buckets: Vec<Bucket> = test::read_body_json(get_all_buckets_resp).await;
+//         assert_eq!(get_all_buckets.len(), 1);
+//
+//         // Get bucket request by bucket name
+//         let get_bucket_resp = TestRequest::get()
+//             .uri(&format!("/searcher/bucket/{}", test_bucket_name))
+//             .send_request(&test_app)
+//             .await;
+//
+//         let get_bucket: Bucket = test::read_body_json(get_bucket_resp).await;
+//         assert_eq!(get_bucket.index, test_bucket_name);
+//
+//         // Delete bucket by index
+//         let delete_bucket_resp = TestRequest::delete()
+//             .uri(&format!("/searcher/bucket/{}", test_bucket_name))
+//             .send_request(&test_app)
+//             .await;
+//
+//         let delete_bucket: SuccessfulResponse = test::read_body_json(delete_bucket_resp).await;
+//         assert_eq!(delete_bucket.code, 200);
+//
+//         // Get bucket by index -> get error message
+//         let get_bucket_err_resp = TestRequest::get()
+//             .uri(&format!("/searcher/bucket/{}", "lsdfnbsikdjfsidg"))
+//             .send_request(&test_app)
+//             .await;
+//
+//         let get_bucket_err: ErrorResponse = test::read_body_json(get_bucket_err_resp).await;
+//         assert_eq!(get_bucket_err.code, 400);
+//     }
+//
+//     #[test]
+//     async fn create_buckets_integration_test() {
+//         let service_parameters = init_service_parameters().unwrap();
+//         let es_host = service_parameters.es_host();
+//         let es_user = service_parameters.es_user();
+//         let es_passwd = service_parameters.es_passwd();
+//
+//         let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
+//         let cxt = ElasticContext::_new(elastic);
+//         let app = App::new()
+//             .app_data(web::Data::new(cxt))
+//             .service(build_service());
+//
+//         let test_app = test::init_service(app).await;
+//         let test_bucket_name = "docs";
+//
+//         // Create new bucket with name: "test_bucket"
+//         let create_bucket_resp = TestRequest::post()
+//             .uri("/searcher/bucket/new")
+//             .set_json(&json!({"bucket_name": test_bucket_name}))
+//             .send_request(&test_app)
+//             .await;
+//
+//         let new_bucket: SuccessfulResponse = test::read_body_json(create_bucket_resp).await;
+//         assert_eq!(new_bucket.code, 200);
+//     }
+// }
--- src/endpoints/searcher.rs
+++ src/endpoints/searcher.rs
@@ -27,185 +27,185 @@ async fn search_target(
     client.search_bucket(buckets.as_str(), &search_form).await
 }
 
-#[cfg(test)]
-mod searcher_endpoints {
-    use crate::searcher::elastic::build_elastic_client;
-    use crate::searcher::elastic::context::ElasticContext;
-    use crate::service::{build_service, init_service_parameters};
-    use crate::wrappers::document::Document;
-    use crate::wrappers::search_params::*;
-
-    use actix_web::test::TestRequest;
-    use actix_web::{test, web, App};
-    use elasticsearch::SearchParts;
-    use serde_json::{json, Value};
-
-    #[test]
-    async fn build_application() {
-        let service_parameters = init_service_parameters().unwrap();
-        let es_host = service_parameters.es_host();
-        let es_user = service_parameters.es_user();
-        let es_passwd = service_parameters.es_passwd();
-
-        let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
-        let cxt = ElasticContext::_new(elastic);
-        let app = App::new()
-            .app_data(web::Data::new(cxt))
-            .service(build_service());
-
-        let test_app = test::init_service(app).await;
-        let test_bucket_name = "test_bucket";
-        for document_index in 1..5 {
-            let document_size = 1024 + document_index;
-            let test_document_name = &format!("test_document_{}", document_index);
-            let _create_document_resp = TestRequest::post()
-                .uri("/searcher/document/new")
-                .set_json(&json!({
-                    "bucket_uuid": test_bucket_name,
-                    "bucket_path": "/tmp/test_document",
-                    "document_name": test_document_name,
-                    "document_path": "/tmp/dir/",
-                    "document_size": document_size,
-                    "document_type": "document",
-                    "document_extension": ".docx",
-                    "document_permissions": 777,
-                    "document_created": "2023-09-15T00:00:00Z",
-                    "document_modified": "2023-09-15T00:00:00Z",
-                    "document_md5_hash": test_document_name,
-                    "document_ssdeep_hash": "3a:34gh5",
-                    "entity_data": "Using skip_serializing does not skip deserializing the field.",
-                    "entity_keywords": ["document", "report"]
-                }))
-                .send_request(&test_app)
-                .await;
-        }
-
-        // Found documents request by document name
-        let mut search_params = SearchParams::default();
-        search_params.query = "document".to_string();
-        let search_resp = TestRequest::post()
-            .uri("/searcher/search")
-            .set_json(&search_params)
-            .send_request(&test_app)
-            .await;
-
-        let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
-        assert_eq!(founded_documents.len() > 0, true);
-
-        // Found documents request by document name with filter
-        let mut search_params = SearchParams::default();
-        search_params.query = "document".to_string();
-        search_params.document_size_from = 1026;
-        let search_resp = TestRequest::post()
-            .uri("/searcher/search")
-            .set_json(&search_params)
-            .send_request(&test_app)
-            .await;
-
-        let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
-        assert_eq!(founded_documents.len() >= 1, true);
-
-        // Found documents request by document name and bucket name
-        let mut search_params = SearchParams::default();
-        search_params.query = "document".to_string();
-        let search_resp = TestRequest::post()
-            .uri(&format!("/searcher/search/{}", test_bucket_name))
-            .set_json(&search_params)
-            .send_request(&test_app)
-            .await;
-
-        let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
-        assert_eq!(founded_documents.len() >= 4, true);
-
-        // Found documents request by document name and bucket name
-        let mut search_params = SearchParams::default();
-        search_params.query = "does not skip".to_string();
-        let search_resp = TestRequest::post()
-            .uri("/searcher/search")
-            .set_json(&search_params)
-            .send_request(&test_app)
-            .await;
-
-        let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
-        assert_eq!(founded_documents.len() >= 4, true);
-    }
-
-    #[test]
-    async fn exec_search_query() {
-        let service_parameters = init_service_parameters().unwrap();
-        let es_host = service_parameters.es_host();
-        let es_user = service_parameters.es_user();
-        let es_passwd = service_parameters.es_passwd();
-
-        let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
-        let cxt = ElasticContext::_new(elastic.clone());
-        let app = App::new()
-            .app_data(web::Data::new(cxt))
-            .service(build_service());
-
-        let _test_app = test::init_service(app).await;
-
-        let query_object = json!({
-            "query": {
-                "bool": {
-                    "must": {
-                        "multi_match": {
-                            "query": "fuzzing binary file using aflgow"
-                        }
-                    },
-                    "filter": {
-                        "bool": {
-                            "must": [
-
-                                {
-                                    "range": {
-                                        "document_size": {
-                                            "gte": 10000
-                                        },
-                                    },
-                                },
-
-                                {
-                                    "term": {
-                                        "document_extension": "txt",
-                                    },
-                                },
-
-
-                                {
-                                    "term": {
-                                        "document_path": "*",
-                                    },
-                                },
-                            ]
-                        }
-                    }
-                }
-            },
-            "highlight" : {
-                "order": "score",
-                "fields" : {
-                    "entity_data": {
-                        "pre_tags" : [""],
-                        "post_tags" : [""]
-                    }
-                }
-            }
-        });
-
-        let result_size = 25;
-        let result_offset = 0;
-        let response_result = elastic
-            .search(SearchParts::Index(&[&"*"]))
-            .from(result_offset)
-            .size(result_size)
-            .body(query_object)
-            .send()
-            .await;
-
-        let response = response_result.unwrap();
-        let json_response = response.json::<Value>().await.unwrap();
-        let json_string = serde_json::to_string_pretty(&json_response);
-        println!("{}", json_string.unwrap().to_string());
-    }
-}
+// #[cfg(test)]
+// mod searcher_endpoints {
+//     use crate::searcher::elastic::build_elastic_client;
+//     use crate::searcher::elastic::context::ElasticContext;
+//     use crate::service::{build_service, init_service_parameters};
+//     use crate::wrappers::document::Document;
+//     use crate::wrappers::search_params::*;
+//
+//     use actix_web::test::TestRequest;
+//     use actix_web::{test, web, App};
+//     use elasticsearch::SearchParts;
+//     use serde_json::{json, Value};
+//
+//     #[test]
+//     async fn build_application() {
+//         let service_parameters = init_service_parameters().unwrap();
+//         let es_host = service_parameters.es_host();
+//         let es_user = service_parameters.es_user();
+//         let es_passwd = service_parameters.es_passwd();
+//
+//         let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
+//         let cxt = ElasticContext::_new(elastic);
+//         let app = App::new()
+//             .app_data(web::Data::new(cxt))
+//             .service(build_service());
+//
+//         let test_app = test::init_service(app).await;
+//         let test_bucket_name = "test_bucket";
+//         for document_index in 1..5 {
+//             let document_size = 1024 + document_index;
+//             let test_document_name = &format!("test_document_{}", document_index);
+//             let _create_document_resp = TestRequest::post()
+//                 .uri("/searcher/document/new")
+//                 .set_json(&json!({
+//                     "bucket_uuid": test_bucket_name,
+//                     "bucket_path": "/tmp/test_document",
+//                     "document_name": test_document_name,
+//                     "document_path": "/tmp/dir/",
+//                     "document_size": document_size,
+//                     "document_type": "document",
+//                     "document_extension": ".docx",
+//                     "document_permissions": 777,
+//                     "document_created": "2023-09-15T00:00:00Z",
+//                     "document_modified": "2023-09-15T00:00:00Z",
+//                     "document_md5_hash": test_document_name,
+//                     "document_ssdeep_hash": "3a:34gh5",
+//                     "entity_data": "Using skip_serializing does not skip deserializing the field.",
+//                     "entity_keywords": ["document", "report"]
+//                 }))
+//                 .send_request(&test_app)
+//                 .await;
+//         }
+//
+//         // Found documents request by document name
+//         let mut search_params = SearchParams::default();
+//         search_params.query = "document".to_string();
+//         let search_resp = TestRequest::post()
+//             .uri("/searcher/search")
+//             .set_json(&search_params)
+//             .send_request(&test_app)
+//             .await;
+//
+//         let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
+//         assert_eq!(founded_documents.len() > 0, true);
+//
+//         // Found documents request by document name with filter
+//         let mut search_params = SearchParams::default();
+//         search_params.query = "document".to_string();
+//         search_params.document_size_from = 1026;
+//         let search_resp = TestRequest::post()
+//             .uri("/searcher/search")
+//             .set_json(&search_params)
+//             .send_request(&test_app)
+//             .await;
+//
+//         let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
+//         assert_eq!(founded_documents.len() >= 1, true);
+//
+//         // Found documents request by document name and bucket name
+//         let mut search_params = SearchParams::default();
+//         search_params.query = "document".to_string();
+//         let search_resp = TestRequest::post()
+//             .uri(&format!("/searcher/search/{}", test_bucket_name))
+//             .set_json(&search_params)
+//             .send_request(&test_app)
+//             .await;
+//
+//         let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
+//         assert_eq!(founded_documents.len() >= 4, true);
+//
+//         // Found documents request by document name and bucket name
+//         let mut search_params = SearchParams::default();
+//         search_params.query = "does not skip".to_string();
+//         let search_resp = TestRequest::post()
+//             .uri("/searcher/search")
+//             .set_json(&search_params)
+//             .send_request(&test_app)
+//             .await;
+//
+//         let founded_documents: Vec<Document> = test::read_body_json(search_resp).await;
+//         assert_eq!(founded_documents.len() >= 4, true);
+//     }
+//
+//     #[test]
+//     async fn exec_search_query() {
+//         let service_parameters = init_service_parameters().unwrap();
+//         let es_host = service_parameters.es_host();
+//         let es_user = service_parameters.es_user();
+//         let es_passwd = service_parameters.es_passwd();
+//
+//         let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
+//         let cxt = ElasticContext::_new(elastic.clone());
+//         let app = App::new()
+//             .app_data(web::Data::new(cxt))
+//             .service(build_service());
+//
+//         let _test_app = test::init_service(app).await;
+//
+//         let query_object = json!({
+//             "query": {
+//                 "bool": {
+//                     "must": {
+//                         "multi_match": {
+//                             "query": "fuzzing binary file using aflgow"
+//                         }
+//                     },
+//                     "filter": {
+//                         "bool": {
+//                             "must": [
+//
+//                                 {
+//                                     "range": {
+//                                         "document_size": {
+//                                             "gte": 10000
+//                                         },
+//                                     },
+//                                 },
+//
+//                                 {
+//                                     "term": {
+//                                         "document_extension": "txt",
+//                                     },
+//                                 },
+//
+//
+//                                 {
+//                                     "term": {
+//                                         "document_path": "*",
+//                                     },
+//                                 },
+//                             ]
+//                         }
+//                     }
+//                 }
+//             },
+//             "highlight" : {
+//                 "order": "score",
+//                 "fields" : {
+//                     "entity_data": {
+//                         "pre_tags" : [""],
+//                         "post_tags" : [""]
+//                     }
+//                 }
+//             }
+//         });
+//
+//         let result_size = 25;
+//         let result_offset = 0;
+//         let response_result = elastic
+//             .search(SearchParts::Index(&[&"*"]))
+//             .from(result_offset)
+//             .size(result_size)
+//             .body(query_object)
+//             .send()
+//             .await;
+//
+//         let response = response_result.unwrap();
+//         let json_response = response.json::<Value>().await.unwrap();
+//         let json_string = serde_json::to_string_pretty(&json_response);
+//         println!("{}", json_string.unwrap().to_string());
+//     }
+// }
