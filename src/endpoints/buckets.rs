04cc058e75beeed6533d5d58b40aad3f930fb3a3
breadrock <breadrock1@gmail.com>
2024-01-16 15:24:54 +0300

    chore(endpoints): re-impled tests for bucket endpoints

--- src/endpoints/buckets.rs
+++ src/endpoints/buckets.rs
@@ -38,105 +38,54 @@ async fn get_bucket(cxt: ContextData, path: web::Path<String>) -> WebResponse<we
     client.get_bucket(bucket_name.as_str()).await
 }
 
-// #[cfg(test)]
-// mod buckets_endpoints {
-//     use crate::errors::{ErrorResponse, SuccessfulResponse};
-//     use crate::searcher::elastic::build_elastic_client;
-//     use crate::searcher::elastic::context::ElasticContext;
-//     use crate::service::{build_service, init_service_parameters};
-//     use crate::wrappers::bucket::Bucket;
-//
-//     use actix_web::test::TestRequest;
-//     use actix_web::{test, web, App};
-//     use serde_json::json;
-//
-//     #[test]
-//     async fn build_application() {
-//         let service_parameters = init_service_parameters().unwrap();
-//         let es_host = service_parameters.es_host();
-//         let es_user = service_parameters.es_user();
-//         let es_passwd = service_parameters.es_passwd();
-//
-//         let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
-//         let cxt = ElasticContext::_new(elastic);
-//         let app = App::new()
-//             .app_data(web::Data::new(cxt))
-//             .service(build_service());
-//
-//         let test_app = test::init_service(app).await;
-//         let test_bucket_name = "test_bucket";
-//
-//         // Create new bucket with name: "test_bucket"
-//         let create_bucket_resp = TestRequest::post()
-//             .uri("/searcher/bucket/new")
-//             .set_json(&json!({"bucket_name": test_bucket_name}))
-//             .send_request(&test_app)
-//             .await;
-//
-//         let new_bucket: SuccessfulResponse = test::read_body_json(create_bucket_resp).await;
-//         assert_eq!(new_bucket.code, 200);
-//
-//         // Get all buckets request
-//         let get_all_buckets_resp = TestRequest::get()
-//             .uri("/searcher/buckets")
-//             .send_request(&test_app)
-//             .await;
-//
-//         let get_all_buckets: Vec<Bucket> = test::read_body_json(get_all_buckets_resp).await;
-//         assert_eq!(get_all_buckets.len(), 1);
-//
-//         // Get bucket request by bucket name
-//         let get_bucket_resp = TestRequest::get()
-//             .uri(&format!("/searcher/bucket/{}", test_bucket_name))
-//             .send_request(&test_app)
-//             .await;
-//
-//         let get_bucket: Bucket = test::read_body_json(get_bucket_resp).await;
-//         assert_eq!(get_bucket.index, test_bucket_name);
-//
-//         // Delete bucket by index
-//         let delete_bucket_resp = TestRequest::delete()
-//             .uri(&format!("/searcher/bucket/{}", test_bucket_name))
-//             .send_request(&test_app)
-//             .await;
-//
-//         let delete_bucket: SuccessfulResponse = test::read_body_json(delete_bucket_resp).await;
-//         assert_eq!(delete_bucket.code, 200);
-//
-//         // Get bucket by index -> get error message
-//         let get_bucket_err_resp = TestRequest::get()
-//             .uri(&format!("/searcher/bucket/{}", "lsdfnbsikdjfsidg"))
-//             .send_request(&test_app)
-//             .await;
-//
-//         let get_bucket_err: ErrorResponse = test::read_body_json(get_bucket_err_resp).await;
-//         assert_eq!(get_bucket_err.code, 400);
-//     }
-//
-//     #[test]
-//     async fn create_buckets_integration_test() {
-//         let service_parameters = init_service_parameters().unwrap();
-//         let es_host = service_parameters.es_host();
-//         let es_user = service_parameters.es_user();
-//         let es_passwd = service_parameters.es_passwd();
-//
-//         let elastic = build_elastic_client(es_host, es_user, es_passwd).unwrap();
-//         let cxt = ElasticContext::_new(elastic);
-//         let app = App::new()
-//             .app_data(web::Data::new(cxt))
-//             .service(build_service());
-//
-//         let test_app = test::init_service(app).await;
-//         let test_bucket_name = "docs";
-//
-//         // Create new bucket with name: "test_bucket"
-//         let create_bucket_resp = TestRequest::post()
-//             .uri("/searcher/bucket/new")
-//             .set_json(&json!({"bucket_name": test_bucket_name}))
-//             .send_request(&test_app)
-//             .await;
-//
-//         let new_bucket: SuccessfulResponse = test::read_body_json(create_bucket_resp).await;
-//         assert_eq!(new_bucket.code, 200);
-//     }
-// }
+#[cfg(test)]
+mod buckets_endpoints {
+    use crate::searcher::own_engine::context::OtherContext;
+    use crate::searcher::service_client::ServiceClient;
+    use crate::wrappers::bucket::BucketForm;
+
+    use actix_web::test;
+
+    #[test]
+    async fn test_create_bucket() {
+        let bucket_form = BucketForm::new("test_bucket");
+        let other_context = OtherContext::_new("test".to_string());
+        let response = other_context.create_bucket(&bucket_form).await;
+        assert_eq!(response.status().as_u16(), 200_u16);
+    }
+
+    #[test]
+    async fn test_delete_bucket() {
+        let bucket_form = BucketForm::new("test_bucket");
+        let other_context = OtherContext::_new("test".to_string());
+        let response = other_context.create_bucket(&bucket_form).await;
+        assert_eq!(response.status().as_u16(), 200_u16);
+
+        let response = other_context.delete_bucket("test_bucket").await;
+        assert_eq!(response.status().as_u16(), 200_u16);
+    }
+
+    #[test]
+    async fn test_get_buckets() {
+        let bucket_form = BucketForm::new("test_bucket");
+        let other_context = OtherContext::_new("test".to_string());
+        let response = other_context.create_bucket(&bucket_form).await;
+        assert_eq!(response.status().as_u16(), 200_u16);
+
+        let response = other_context.get_all_buckets().await;
+        let buckets_size = response.unwrap().0.len();
+        assert_eq!(buckets_size, 1);
+    }
+
+    #[test]
+    async fn test_get_bucket_by_id() {
+        let bucket_form = BucketForm::new("test_bucket");
+        let other_context = OtherContext::_new("test".to_string());
+        let response = other_context.create_bucket(&bucket_form).await;
+        assert_eq!(response.status().as_u16(), 200_u16);
+
+        let get_bucket_result = other_context.get_bucket("test_bucket").await;
+        let bucket_uuid = &get_bucket_result.unwrap().uuid;
+        assert_eq!(bucket_uuid.as_str(), "test_bucket");
+    }
+}
