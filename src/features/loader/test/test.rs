ae8b93afea9ec996bacc81e658ac3fddceabece8
breadrock <breadrock1@gmail.com>
2024-01-16 15:20:24 +0300

    chore(loader): code style fixes into loadel feature

--- src/features/loader/test/test.rs
+++ src/features/loader/test/test.rs
@@ -8,7 +8,7 @@ mod tests {
     fn load_directory_entity_test() {
         let file_path = "./src/features/loader/resources";
         let path_object = Path::new(file_path);
-        let documents = loader::load_directory_entity(&path_object)
+        let documents = loader::load_passed_file_by_path(&path_object)
             .into_iter()
             .map(|file_data| file_data.document_name.clone())
             .collect::<Vec<String>>();
@@ -20,7 +20,7 @@ mod tests {
     fn load_file_entity_test() {
         let file_path = "./src/features/loader/resources/file_1.txt";
         let path_object = Path::new(file_path);
-        let documents = loader::load_directory_entity(&path_object)
+        let documents = loader::load_passed_file_by_path(&path_object)
             .into_iter()
             .map(|file_data| file_data.document_name.clone())
             .collect::<Vec<String>>();
