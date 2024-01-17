ff86e6e60263c9a6aaa637641cdb03715d8054c0
breadrock <breadrock1@gmail.com>
2024-01-17 20:06:10 +0300

    chore(searcher): added download file functionality to endpoints and service

--- src/service.rs
+++ src/service.rs
@@ -4,7 +4,8 @@ use crate::endpoints::buckets::{
 use crate::endpoints::clusters::{all_clusters, delete_cluster, get_cluster, new_cluster};
 use crate::endpoints::documents::{delete_document, get_document, new_document, update_document};
 use crate::endpoints::hello::hello;
-use crate::endpoints::loader::load_file;
+
+use crate::endpoints::loader::{load_file, download_file};
 use crate::endpoints::searcher::{search_all, search_target};
 use crate::endpoints::similarities::{search_similar_docs, search_similar_docs_target};
 
@@ -117,4 +118,5 @@ pub fn build_service() -> Scope {
         .service(search_similar_docs)
         .service(search_similar_docs_target)
         .service(load_file)
+        .service(download_file)
 }
