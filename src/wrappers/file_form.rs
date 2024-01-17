04f6814c39bc8dbce6036197056fc9d1699d63e6
breadrock <breadrock1@gmail.com>
2024-01-17 20:04:17 +0300

    chore(wrappers): removed useless code from file_form module

--- src/wrappers/file_form.rs
+++ src/wrappers/file_form.rs
@@ -1,11 +1,4 @@
-use actix_multipart::form::tempfile::TempFile;
-use actix_multipart::form::MultipartForm;
 use serde_derive::Deserialize;
-
-#[derive(MultipartForm)]
-pub struct UploadFileForm {
-    _file: TempFile,
-}
 
 #[derive(Deserialize)]
 pub struct LoadFileForm {
@@ -26,34 +19,3 @@ impl LoadFileForm {
         }
     }
 }
-
-// pub async fn upload_file() {
-//     const MAX_FILE_SIZE: u64 = 1024 * 1024 * 10; // 10 MB
-//     const MAX_FILE_COUNT: i32 = 1;
-//
-//     // reject malformed requests
-//     match form.file.size {
-//         0 => return HttpResponse::BadRequest().finish(),
-//         length if length > MAX_FILE_SIZE.try_into().unwrap() => {
-//             return HttpResponse::BadRequest()
-//                 .body(format!("The uploaded file is too large. Maximum size is {} bytes.", MAX_FILE_SIZE));
-//         },
-//         _ => {}
-//     };
-//
-//     let temp_file_path = form.file.file.path();
-//     let file_name: &str = form
-//         .file
-//         .file_name
-//         .as_ref()
-//         .map(|m| m.as_ref())
-//         .unwrap_or("null");
-//
-//     let mut file_path = PathBuf::from(&config.data_path);
-//     file_path.push(&sanitize_filename::sanitize(&file_name));
-//
-//     match std::fs::rename(temp_file_path, file_path) {
-//         Ok(_) => HttpResponse::Ok().finish(),
-//         Err(_) => HttpResponse::InternalServerError().finish(),
-//     }
-// }
