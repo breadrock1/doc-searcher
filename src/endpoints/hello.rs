c9761dccb7dfe2097bb13814ee91a2e5a1b665c0
breadrock <breadrock1@gmail.com>
2024-01-18 13:38:38 +0300

    feat(endpoints): appended utoipa derive macros for hello endpoints

--- src/endpoints/hello.rs
+++ src/endpoints/hello.rs
@@ -1,8 +1,17 @@
 use crate::endpoints::ContextData;
-use crate::errors::SuccessfulResponse;
+use crate::errors::*;
 
 use actix_web::{get, HttpResponse};
 
+#[utoipa::path(
+    get,
+    path = "/searcher/hello",
+    tag = "Test server connection endpoint",
+    responses(
+        (status = 200, description = "Successful", body = SuccessfulResponse),
+        (status = 501, description = "Server does not available", body = ErrorResponse),
+    ),
+)]
 #[get("/hello")]
 async fn hello(cxt: ContextData) -> HttpResponse {
     let _client = cxt.get_ref();
