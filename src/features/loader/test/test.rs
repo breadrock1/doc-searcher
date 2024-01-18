81f5ec4d958da87877fb9b373459aa8a789ffb5d
breadrock <breadrock1@gmail.com>
2024-01-18 09:53:21 +0300

    chore(loader): updated tests for loader crate

--- src/features/loader/test/test.rs
+++ src/features/loader/test/test.rs
@@ -6,25 +6,65 @@ mod tests {
 
     #[test]
     fn load_directory_entity_test() {
-        let file_path = "./src/features/loader/resources";
+        let file_path = "./resources";
         let path_object = Path::new(file_path);
         let documents = loader::load_passed_file_by_path(&path_object)
             .into_iter()
             .map(|file_data| file_data.document_name.clone())
             .collect::<Vec<String>>();
 
-        assert_eq!("file_1.txt", documents.first().unwrap().as_str());
+        assert_eq!("test.txt", documents.first().unwrap().as_str());
     }
 
     #[test]
-    fn load_file_entity_test() {
-        let file_path = "./src/features/loader/resources/file_1.txt";
+    fn test_load_txt_file_entity() {
+        let file_path = "./src/features/loader/resources/test.txt";
         let path_object = Path::new(file_path);
         let documents = loader::load_passed_file_by_path(&path_object)
             .into_iter()
-            .map(|file_data| file_data.document_name.clone())
+            .map(|file_data| file_data.entity_data.clone())
             .collect::<Vec<String>>();
 
-        assert_eq!("file_1.txt", documents.first().unwrap().as_str());
+        let extracted_data_size = documents.first().unwrap().len();
+        assert_eq!(extracted_data_size, 5891);
+    }
+
+    #[test]
+    fn test_load_docx_file_entity() {
+        let file_path = "./src/features/loader/resources/test.docx";
+        let path_object = Path::new(file_path);
+        let documents = loader::load_passed_file_by_path(&path_object)
+            .into_iter()
+            .map(|file_data| file_data.entity_data.clone())
+            .collect::<Vec<String>>();
+
+        let extracted_data_size = documents.first().unwrap().len();
+        assert_eq!(extracted_data_size, 5633);
+    }
+
+    #[test]
+    fn test_load_excel_file_entity() {
+        let file_path = "./src/features/loader/resources/test.xls";
+        let path_object = Path::new(file_path);
+        let documents = loader::load_passed_file_by_path(&path_object)
+            .into_iter()
+            .map(|file_data| file_data.entity_data.clone())
+            .collect::<Vec<String>>();
+
+        let extracted_data_size = documents.first().unwrap().len();
+        assert_eq!(extracted_data_size, 542);
+    }
+
+    #[test]
+    fn test_load_pdf_file_entity() {
+        let file_path = "./src/features/loader/resources/test.pdf";
+        let path_object = Path::new(file_path);
+        let documents = loader::load_passed_file_by_path(&path_object)
+            .into_iter()
+            .map(|file_data| file_data.entity_data.clone())
+            .collect::<Vec<String>>();
+
+        let extracted_data_size = documents.first().unwrap().len();
+        assert_eq!(extracted_data_size, 6345);
     }
 }
