ce3e24ef77430aa11734f2295e65bd3117f6723f
breadrock <breadrock1@gmail.com>
2024-01-18 13:38:42 +0300

    feat(endpoints): appended utoipa derive macros for loader endpoints

--- src/endpoints/loader.rs
+++ src/endpoints/loader.rs
@@ -2,8 +2,18 @@ use crate::endpoints::ContextData;
 use crate::wrappers::file_form::LoadFileForm;
 
 use actix_web::{post, web};
-use actix_web::{HttpResponse, HttpRequest};
+use actix_web::{HttpRequest, HttpResponse};
 
+#[utoipa::path(
+    post,
+    path = "/searcher/loader/load",
+    tag = "Load file from local file system of service by path",
+    request_body = LoadFileForm,
+    responses(
+        (status = 200, description = "Successful", body = SuccessfulResponse),
+        (status = 401, description = "Failed while loading files", body = ErrorResponse),
+    )
+)]
 #[post("/loader/load")]
 async fn load_file(cxt: ContextData, form: web::Json<LoadFileForm>) -> HttpResponse {
     let client = cxt.get_ref();
@@ -12,12 +22,27 @@ async fn load_file(cxt: ContextData, form: web::Json<LoadFileForm>) -> HttpRespo
     client.load_file_to_bucket(bucket_name, file_path).await
 }
 
+#[utoipa::path(
+    post,
+    path = "/searcher/loader/load-file",
+    tag = "Download file from local system by path",
+    request_body = LoadFileForm,
+    responses(
+        (status = 200, description = "Successful", body = SuccessfulResponse),
+        (status = 401, description = "Failed while downloading files", body = ErrorResponse),
+    )
+)]
 #[post("/loader/load-file")]
-async fn download_file(cxt: ContextData, req: HttpRequest, form: web::Json<LoadFileForm>) -> HttpResponse {
+async fn download_file(
+    cxt: ContextData,
+    req: HttpRequest,
+    form: web::Json<LoadFileForm>,
+) -> HttpResponse {
     let client = cxt.get_ref();
     let file_path = form.get_path();
     let bucket_name = form.get_bucket();
-    client.download_file(bucket_name, file_path)
+    client
+        .download_file(bucket_name, file_path)
         .await
         .unwrap()
         .into_response(&req)
