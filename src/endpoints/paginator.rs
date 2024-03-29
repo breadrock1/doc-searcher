2d56996b8e0b742ca22cbdbf48ee0c7ff2e00a81
breadrock <breadrock1@gmail.com>
2024-03-29 13:01:29 +0300

    chore(swagger): Added pagination endpoint to swagger docs

--- src/endpoints/paginator.rs
+++ src/endpoints/paginator.rs
@@ -5,12 +5,31 @@ use actix_web::{delete, get, post, web, HttpResponse};
 use wrappers::document::Document;
 use wrappers::scroll::{AllScrolls, NextScroll};
 
+#[utoipa::path(
+    get,
+    path = "/pagination/",
+    tag = "Get all available pagination sessions",
+    responses(
+        (status = 200, description = "Successful", body = [String]),
+        (status = 401, description = "Failed while getting all pagination sessions", body = ErrorResponse),
+    )
+)]
 #[get("/")]
 async fn get_pagination_ids(cxt: SearcherData) -> JsonResponse<Vec<String>> {
     let client = cxt.get_ref();
     client.get_pagination_ids().await
 }
 
+#[utoipa::path(
+    delete,
+    path = "/pagination/",
+    tag = "Delete passed pagination sessions dy id",
+    request_body = AllScrolls,
+    responses(
+        (status = 200, description = "Successful", body = SuccessfulResponse),
+        (status = 401, description = "Failed while deleting pagination sessions", body = ErrorResponse),
+    )
+)]
 #[delete("/")]
 async fn delete_expired_ids(cxt: SearcherData, form: web::Json<AllScrolls>) -> HttpResponse {
     let client = cxt.get_ref();
@@ -18,6 +37,16 @@ async fn delete_expired_ids(cxt: SearcherData, form: web::Json<AllScrolls>) -> H
     client.delete_pagination_ids(&pagination_form).await
 }
 
+#[utoipa::path(
+    post,
+    path = "/pagination/next",
+    tag = "Next scroll of searching results",
+    request_body = NextScroll,
+    responses(
+        (status = 200, description = "Successful", body = SuccessfulResponse),
+        (status = 401, description = "Failed while scrolling", body = ErrorResponse),
+    )
+)]
 #[post("/next")]
 async fn next_pagination_result(
     cxt: SearcherData,
