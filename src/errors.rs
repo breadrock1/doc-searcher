a993090742147f0c65a748102d428608ed371199
Bread White <breadrock1@gmail.com>
2024-05-12 20:01:28 +0300

    chore(errors): Refactored errors for services

--- src/errors.rs
+++ src/errors.rs
@@ -1,75 +1,87 @@
+use wrappers::scroll::PaginatedResult;
+
 use actix_web::http::StatusCode;
 use actix_web::{web, HttpResponse, ResponseError};
+use elasticsearch::http::response::Exception;
 use serde::{Deserialize, Serialize};
+use std::io::Error;
 use thiserror::Error;
 use utoipa::ToSchema;
-use wrappers::scroll::PaginatedResult;
 
 pub(crate) type JsonResponse<T> = Result<web::Json<T>, WebError>;
 pub(crate) type PaginateJsonResponse<T> = JsonResponse<PaginatedResult<T>>;
 
 #[derive(Error, Debug)]
 pub enum WebError {
-    #[error("Error while getting cluster: {0}")]
+    #[error("Failed to get all clusters: {0}")]
+    GetClusters(String),
+    #[error("Failed to get cluster details: {0}")]
     GetCluster(String),
-    #[error("Error while creating cluster: {0}")]
+    #[error("Failed to create new cluster: {0}")]
     CreateCluster(String),
-    #[error("Error while deleting cluster: {0}")]
-    DeletingCluster(String),
-    #[error("Error while getting bucket: {0}")]
-    GetBucket(String),
-    #[error("Error while creating bucket: {0}")]
-    CreateBucket(String),
-    #[error("Error while deleting bucket: {0}")]
-    DeleteBucket(String),
-    #[error("Error elasticsearch response: {0}")]
+    #[error("Failed to delete cluster: {0}")]
+    DeleteCluster(String),
+    #[error("Failed to get all folders: {0}")]
+    GetFolders(String),
+    #[error("Failed to get folder details: {0}")]
+    GetFolder(String),
+    #[error("Failed to create new folder: {0}")]
+    CreateFolder(String),
+    #[error("Failed to delete folder: {0}")]
+    DeleteFolder(String),
+    #[error("Failed to get document: {0}")]
     GetDocument(String),
-    #[error("Failed while creating document: {0}")]
+    #[error("Failed to create new document: {0}")]
     CreateDocument(String),
-    #[error("Failed while updating document: {0}")]
-    UpdateDocument(String),
-    #[error("Failed while deleting document: {0}")]
+    #[error("Failed to delete document: {0}")]
     DeleteDocument(String),
-    #[error("Failed while serializing document: {0}")]
-    DocumentSerializing(String),
+    #[error("Failed to update document: {0}")]
+    UpdateDocument(String),
+    #[error("Failed to move documents to folder: {0}")]
+    MoveDocuments(String),
+    #[error("Failed to (de)serialize object: {0}")]
+    SerdeError(String),
     #[error("Failed while searching: {0}")]
-    SearchFailed(String),
-    #[error("Passed file path does not exist: {0}")]
-    LoadFileFailed(String),
+    SearchError(String),
+    #[error("Error response from searcher service: {0}")]
+    SearchServiceError(String),
+    #[error("Failed to upload file: {0}")]
+    UploadFileError(String),
+    #[error("Failed while paginating: {0}")]
+    PaginationError(String),
+    #[error("Service unavailable: {0}")]
+    ServiceUnavailable(String),
     #[error("Response error: {0}")]
-    ResponseError(String),
+    UnknownError(String),
     #[error("Continues executing: {0}")]
     ResponseContinues(String),
 }
 
 impl WebError {
-    pub fn name(&self) -> String {
+    pub fn name(&self) -> &str {
         match self {
-            WebError::SearchFailed(_) => "SearchError",
-            WebError::ResponseError(_) => "ResponseError",
-            WebError::GetBucket(_) => "GetBucketError",
-            WebError::GetCluster(_) => "GetClusterError",
-            WebError::GetDocument(_) => "GetDocumentError",
-            WebError::CreateDocument(_) => "CreateDocumentError",
-            WebError::UpdateDocument(_) => "UpdateDocumentError",
-            WebError::DeleteDocument(_) => "DeleteDocumentError",
-            WebError::DocumentSerializing(_) => "DocumentSerializingError",
-            WebError::ResponseContinues(_) => "ContinueExecutingResponse",
-            _ => "RuntimeError",
+            WebError::GetClusters(_) => "Get clusters error",
+            WebError::GetCluster(_) => "Get cluster error",
+            WebError::CreateCluster(_) => "Create cluster error",
+            WebError::DeleteCluster(_) => "Delete cluster error",
+            WebError::GetFolders(_) => "Get folders error",
+            WebError::GetFolder(_) => "Get folder error",
+            WebError::CreateFolder(_) => "Create folder error",
+            WebError::DeleteFolder(_) => "Delete folder error",
+            WebError::GetDocument(_) => "Get document error",
+            WebError::CreateDocument(_) => "Create document error",
+            WebError::DeleteDocument(_) => "Delete document error",
+            WebError::UpdateDocument(_) => "Update document error",
+            WebError::MoveDocuments(_) => "Move documents error",
+            WebError::SerdeError(_) => "Serde error",
+            WebError::SearchError(_) => "Search data error",
+            WebError::SearchServiceError(_) => "Search server error",
+            WebError::UploadFileError(_) => "Upload file error",
+            WebError::PaginationError(_) => "Pagination error",
+            WebError::ServiceUnavailable(_) => "Service unavailable",
+            WebError::ResponseContinues(_) => "Processing...",
+            WebError::UnknownError(_) => "Runtime error",
         }
-        .to_string()
-    }
-}
-
-impl From<elasticsearch::Error> for WebError {
-    fn from(value: elasticsearch::Error) -> Self {
-        WebError::ResponseError(value.to_string())
-    }
-}
-
-impl From<serde_json::Error> for WebError {
-    fn from(value: serde_json::Error) -> Self {
-        WebError::ResponseError(value.to_string())
     }
 }
 
@@ -83,16 +95,9 @@ pub(crate) struct ErrorResponse {
 impl ResponseError for WebError {
     fn status_code(&self) -> StatusCode {
         match self {
-            WebError::SearchFailed(_) => StatusCode::BAD_REQUEST,
-            WebError::ResponseError(_) => StatusCode::BAD_REQUEST,
-            WebError::GetBucket(_) => StatusCode::BAD_REQUEST,
-            WebError::GetCluster(_) => StatusCode::BAD_REQUEST,
-            WebError::GetDocument(_) => StatusCode::BAD_REQUEST,
-            WebError::CreateDocument(_) => StatusCode::BAD_REQUEST,
-            WebError::UpdateDocument(_) => StatusCode::BAD_REQUEST,
-            WebError::DeleteDocument(_) => StatusCode::BAD_REQUEST,
-            WebError::DocumentSerializing(_) => StatusCode::BAD_REQUEST,
             WebError::ResponseContinues(_) => StatusCode::PROCESSING,
+            WebError::SerdeError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            WebError::UnknownError(_) => StatusCode::INTERNAL_SERVER_ERROR,
             _ => StatusCode::BAD_REQUEST,
         }
     }
@@ -102,7 +107,7 @@ impl ResponseError for WebError {
         let response = ErrorResponse {
             code: status_code.as_u16(),
             message: self.to_string(),
-            error: self.name(),
+            error: self.name().to_string(),
         };
 
         HttpResponse::build(status_code).json(response)
@@ -110,12 +115,24 @@ impl ResponseError for WebError {
 }
 
 #[derive(Deserialize, Serialize, ToSchema)]
-pub(crate) struct SuccessfulResponse {
+pub struct SuccessfulResponse {
     pub code: u16,
     pub message: String,
 }
 
 impl SuccessfulResponse {
+    pub fn new(code: u16, msg: &str) -> Self {
+        let message = msg.to_string();
+        SuccessfulResponse { code, message }
+    }
+
+    pub fn success(msg: &str) -> Self {
+        SuccessfulResponse {
+            code: 200u16,
+            message: msg.to_string(),
+        }
+    }
+
     pub fn ok_response(msg: &str) -> HttpResponse {
         let status_code = StatusCode::OK;
         let response = SuccessfulResponse {
@@ -128,5 +145,49 @@ impl SuccessfulResponse {
 
     pub fn to_response(&self) -> HttpResponse {
         HttpResponse::build(StatusCode::OK).json(self)
+    }
+
+    pub fn is_success(&self) -> bool {
+        self.code == 200
+    }
+}
+
+impl From<Exception> for WebError {
+    fn from(value: Exception) -> Self {
+        let err_msg = value.error().reason().unwrap();
+        log::error!("{}", err_msg);
+        WebError::UnknownError(err_msg.to_string())
+    }
+}
+
+impl From<elasticsearch::Error> for WebError {
+    fn from(value: elasticsearch::Error) -> Self {
+        let err_msg = value.to_string();
+        log::error!("{}", err_msg.as_str());
+        WebError::SearchServiceError(err_msg)
+    }
+}
+
+impl From<serde_json::Error> for WebError {
+    fn from(value: serde_json::Error) -> Self {
+        let err_msg = value.to_string();
+        log::error!("{}", err_msg.as_str());
+        WebError::UnknownError(err_msg)
+    }
+}
+
+impl From<std::io::Error> for WebError {
+    fn from(value: Error) -> Self {
+        let err_msg = value.to_string();
+        log::error!("{}", err_msg.as_str());
+        WebError::UploadFileError(err_msg)
+    }
+}
+
+impl From<reqwest::Error> for WebError {
+    fn from(value: reqwest::Error) -> Self {
+        let err_msg = value.to_string();
+        log::error!("{}", err_msg.as_str());
+        WebError::ServiceUnavailable(err_msg)
     }
 }
