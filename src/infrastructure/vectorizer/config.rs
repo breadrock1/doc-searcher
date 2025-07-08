9ce6c119b258bf7d81471e9c2cf5b40bc57b8cb4
breadrock1 <breadrock1@gmail.com>
2025-07-08 15:10:24 +0300

    merge: merged all

--- src/infrastructure/vectorizer/config.rs
+++ src/infrastructure/vectorizer/config.rs
@@ -2,11 +2,7 @@ use getset::{CopyGetters, Getters};
 use serde_derive::Deserialize;
 
 #[derive(Clone, Deserialize, CopyGetters, Getters)]
-<<<<<<<< HEAD:src/infrastructure/vectorizer/config.rs
-pub struct VectorizerConfig {
-========
 pub struct BAAIConfig {
->>>>>>>> master:src/tokenizer/baai/config.rs
     #[getset(get = "pub")]
     address: String,
     #[getset(get_copy = "pub")]
--- src/server/config.rs
+++
@@ -1,8 +0,0 @@
-use getset::{CopyGetters, Getters};
-use serde::Deserialize;
-
-#[derive(Clone, Deserialize, CopyGetters, Getters)]
-#[getset(get = "pub")]
-pub struct ServerConfig {
-    address: String,
-}
