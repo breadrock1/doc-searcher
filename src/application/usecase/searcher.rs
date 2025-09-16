1dc10effaf9e7f8339b33c220d406e504f0d1d19
Bread White <32078281+breadrock1@users.noreply.github.com>
2025-09-16 12:51:57 -0700

    Improve: Added tracing filter (#56)
    * chore: impled filtering for otlp tracing
    
    * chore: changed visibility for several logs
    
    * chore: impled correct tracing instrument macros
    
    * chore: removed useless prometheus config
    
    * chore: updated project version
    
    * fix: fixed fmt warnings

--- src/application/usecase/searcher.rs
+++ src/application/usecase/searcher.rs
@@ -29,6 +29,7 @@ impl<Searcher> SearcherUseCase<Searcher>
 where
     Searcher: DocumentSearcher + PaginateManager + Send + Sync + Clone,
 {
+    #[tracing::instrument(skip(self), level = "debug")]
     pub async fn retrieve(
         &self,
         ids: &str,
@@ -37,22 +38,27 @@ where
         self.client.retrieve(ids, params).await
     }
 
+    #[tracing::instrument(skip(self), level = "debug")]
     pub async fn fulltext(&self, params: &FullTextSearchParams) -> PaginateResult<FoundedDocument> {
         self.client.fulltext(params).await
     }
 
+    #[tracing::instrument(skip(self), level = "debug")]
     pub async fn semantic(&self, params: &SemanticSearchParams) -> PaginateResult<FoundedDocument> {
         self.client.semantic(params).await
     }
 
+    #[tracing::instrument(skip(self), level = "debug")]
     pub async fn hybrid(&self, params: &HybridSearchParams) -> PaginateResult<FoundedDocument> {
         self.client.hybrid(params).await
     }
 
+    #[tracing::instrument(skip(self), level = "debug")]
     pub async fn paginate(&self, params: &PaginateParams) -> PaginateResult<FoundedDocument> {
         self.client.paginate(params).await
     }
 
+    #[tracing::instrument(skip(self), level = "debug")]
     pub async fn delete_session(&self, session_id: &str) -> StorageResult<()> {
         self.client.delete_session(session_id).await
     }
