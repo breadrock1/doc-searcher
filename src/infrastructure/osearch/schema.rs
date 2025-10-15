607b4b2f9388642853880225ec904b54358fd7d9
breadrock1 <breadrock1@gmail.com>
2025-10-15 13:32:45 +0300

    chore: updated schema

--- src/infrastructure/osearch/schema.rs
+++ src/infrastructure/osearch/schema.rs
@@ -7,7 +7,6 @@ pub const INGEST_PIPELINE_NAME: &str = "embeddings-ingest-pipeline";
 pub const HYBRID_SEARCH_PIPELINE_NAME: &str = "hybrid-search-pipeline";
 const NORMALIZATION_TECHNIQUE: &str = "min_max";
 const COMBINATION_TECHNIQUE: &str = "arithmetic_mean";
-// const KNN_SPACE_TYPE: &str = "cosinesimil";
 const KNN_SPACE_TYPE: &str = "l2";
 const TOKENIZER_KIND: &str = "standard";
 
@@ -91,7 +90,6 @@ pub fn create_document_schema(
         "settings": {
             "index": {
                 "knn": true,
-                "knn.space_type": KNN_SPACE_TYPE,
                 "knn.algo_param.ef_search": knn_params.knn_ef_searcher(),
                 "number_of_shards": config.number_of_shards(),
                 "number_of_replicas": config.number_of_replicas(),
@@ -136,9 +134,6 @@ pub fn create_document_schema(
                                 "engine": "lucene",
                                 "space_type": "l2",
                                 "name": "hnsw",
-                                "parameters": {
-
-                                }
                             }
                         }
                     }
