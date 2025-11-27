63fb2e0252b4052f235e667a544a8cf8966a11e5
breadrock1 <breadrock1@gmail.com>
2025-11-27 10:42:39 +0300

    chore!: impled otlp logs and meters collecting

--- doc-search-otlp/src/config.rs
+++ doc-search-otlp/src/config.rs
@@ -2,25 +2,25 @@ use gset::Getset;
 use serde_derive::Deserialize;
 
 #[derive(Clone, Deserialize, Getset)]
-pub struct OtlpConfig {
+pub struct TelemetryConfig {
+    #[getset(get, vis = "pub")]
+    level: String,
     #[getset(get_copy, vis = "pub")]
     enable_remote_otlp: bool,
     #[getset(get, vis = "pub")]
-    logger: LoggerConfig,
-    #[getset(get, vis = "pub")]
-    tracing: TracingConfig,
+    otlp_address: Option<String>,
 }
 
-#[derive(Clone, Deserialize, Getset)]
-pub struct LoggerConfig {
-    #[getset(get, vis = "pub")]
-    level: String,
-    #[getset(get, vis = "pub")]
-    address: String,
-}
-
-#[derive(Clone, Deserialize, Getset)]
-pub struct TracingConfig {
-    #[getset(get, vis = "pub")]
-    address: String,
-}
+// #[derive(Clone, Deserialize, Getset)]
+// pub struct LoggerConfig {
+//     #[getset(get, vis = "pub")]
+//     level: String,
+//     #[getset(get, vis = "pub")]
+//     address: String,
+// }
+//
+// #[derive(Clone, Deserialize, Getset)]
+// pub struct TracingConfig {
+//     #[getset(get, vis = "pub")]
+//     address: String,
+// }
