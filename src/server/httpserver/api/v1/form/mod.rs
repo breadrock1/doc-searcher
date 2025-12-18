b6a60f0c766994c7b16596015a753e8696c8adc2
breadrock1 <breadrock1@gmail.com>
2025-12-18 15:02:47 +0300

    chore: updated scope of api v1 forms

--- src/server/httpserver/api/v1/form/mod.rs
+++ src/server/httpserver/api/v1/form/mod.rs
@@ -1,11 +1,16 @@
+#![allow(unused_imports)]
+
 mod document;
 pub use document::CreateDocumentForm;
+pub use document::UpdateDocumentForm;
 
 mod index;
-pub use index::{CreateIndexForm, KnnIndexForm};
+pub use index::CreateIndexForm;
+pub use index::KnnIndexForm;
 
 mod search_params;
 pub use search_params::{FilterForm, ResultForm, ShortResultForm};
+pub use search_params::PaginateForm;
 pub use search_params::{
     FullTextSearchForm, HybridSearchForm, RetrieveDocumentForm, SemanticSearchForm,
 };
