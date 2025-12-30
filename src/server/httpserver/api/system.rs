3d51ef8a3c99f91ff25311a300026b3b70ea3814
Bread White <32078281+breadrock1@users.noreply.github.com>
2025-12-30 04:30:03 -0500

    Feature: Added home page to httpserver (#74)
    * feat: added home page to httpserver
    
    * chore: updated minor project version

--- src/server/httpserver/api/system.rs
+++ src/server/httpserver/api/system.rs
@@ -1,6 +1,6 @@
 use axum::extract::State;
 use axum::http::StatusCode;
-use axum::response::IntoResponse;
+use axum::response::{Html, IntoResponse};
 use axum::routing::get;
 use axum::Router;
 use doc_search_core::domain::searcher::{IPaginator, ISearcher};
@@ -11,6 +11,8 @@ use crate::server::{ServerApp, ServerResult};
 
 const HEALTH_URL: &str = "/health";
 const METRICS_URL: &str = "/metrics";
+const HOME_URL: &str = "/";
+const INDEX_HTML_PAGE_DATA: &str = include_str!("../../../../static/index.html");
 
 pub fn init_system_routers<Storage, Searcher>() -> Router<Arc<ServerApp<Storage, Searcher>>>
 where
@@ -19,10 +21,21 @@ where
 {
     // TODO: replace to otlp push model
     let router: Router<Arc<ServerApp<Storage, Searcher>>> = Router::new()
+        .route(HOME_URL, get(home))
         .route(HEALTH_URL, get(health))
         .route(METRICS_URL, get(metrics));
 
     router
+}
+
+pub async fn home<Storage, Searcher>(
+    State(_state): State<Arc<ServerApp<Storage, Searcher>>>,
+) -> Html<String>
+where
+    Searcher: ISearcher + IPaginator + Send + Sync + Clone + 'static,
+    Storage: IIndexStorage + IDocumentPartStorage + Send + Sync + Clone + 'static,
+{
+    Html(INDEX_HTML_PAGE_DATA.to_owned())
 }
 
 pub async fn health<Storage, Searcher>(
